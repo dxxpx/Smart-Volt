b17ff6e454f775f69dfddc3504ed4144b92b00b5
dxxpx <dxxpka.20@gmail.com>
2024-10-19 20:31:07 +0530

    Initialized On another project

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'Authentication/SignInPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter Demo', home: Signinpage());
+  }
+}
